65f12efdd1cf7a78eb095dd4b811c71387c158b9
IceTharu <36802006+IceTharu@users.noreply.github.com>
2019-01-10 13:45:16 +0100

    Add files via upload
    Makefile and libmatmult with matrix allocation

--- libmatmult.c
+++ libmatmult.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include <cblas.h>
-
 #define min(x,y) ((x<y)?x:y)
 
+/* Natural method */
 void matmult_nat(int rows1,int cols2,int cols1,double** matrix1,double** matrix2,double** matrix_out){
-	
-	for(int i = 0; i < rows1; i++){
-		for(int j = 0; j < cols2; j++){
-			for(int k = 0; k < cols1; k++){
 
-				matrix_out[i][j] = matrix_out[i][j] + matrix1[i][k] * matrix2[k][j];
-
-			}
-		}
-	}
+  for(int i = 0; i < rows1; i++){
+    for(int j = 0; j < cols2; j++){
+      matrix_out[i][j] = 0;
+    }
+  }
+  for(int i = 0; i < rows1; i++){
+    for(int j = 0; j < cols2; j++){
+      for(int k = 0; k < cols1; k++){
+	matrix_out[i][j] += matrix1[i][k]*matrix2[k][j];
+      }
+    }
+  }
 }
 
 void matmult_lib(int rows1,int cols2,int cols1,double** matrix1,double** matrix2,double** matrix_out){
@@ -23,126 +26,146 @@ void matmult_lib(int rows1,int cols2,int cols1,double** matrix1,double** matrix2
 
 
 void matmult_mnk(int m,int n,int k,double** matrix1,double** matrix2,double** matrix_out){
-	
-	for(int i = 0; i < m; i++){
-		for(int j = 0; j < n; j++){
-			for(int z = 0; z < k; z++){
 
-				matrix_out[i][j] = matrix_out[i][j] + matrix1[i][z] * matrix2[z][j];
+  for(int i = 0; i < m; i++){
+    for(int j = 0; j < n; j++){
+      matrix_out[i][j] = 0;
+    }
+  }	
 
-			}
-		}
-	}
+  for(int i = 0; i < m; i++){
+    for(int j = 0; j < n; j++){
+      for(int z = 0; z < k; z++){
+	matrix_out[i][j] += matrix1[i][z] * matrix2[z][j];
+      }
+    }
+  }
 }
 
 
 void matmult_nmk(int m,int n,int k,double** matrix1,double** matrix2,double** matrix_out){
-	
-	for(int j = 0; j < n; j++){
-		for(int i = 0; i < m; i++){
-			for(int z = 0; z < k; z++){
 
-				matrix_out[i][j] = matrix_out[i][j] + matrix1[i][z] * matrix2[z][j];
-
-			}
-		}
-	}
+  for(int i = 0; i < m; i++){
+    for(int j = 0; j < n; j++){
+      matrix_out[i][j] = 0;
+    }
+  }	
+  for(int j = 0; j < n; j++){
+    for(int i = 0; i < m; i++){
+      for(int z = 0; z < k; z++){
+	matrix_out[i][j] += matrix1[i][z]*matrix2[z][j];
+      }
+    }
+  }
 }
 
 
 void matmult_kmn(int m,int n,int k,double** matrix1,double** matrix2,double** matrix_out){
+
+  for(int i = 0; i < m; i++){
+    for(int j = 0; j < n; j++){
+      matrix_out[i][j] = 0;
+    }
+  }
 	
-	for(int z = 0; z < k; z++){
-		for(int i = 0; i < m; i++){
-			for(int j = 0; j < n; j++){
-
-				matrix_out[i][j] = matrix_out[i][j] + matrix1[i][z] * matrix2[z][j];
-
-			}
-		}
-	}
+  for(int z = 0; z < k; z++){
+    for(int i = 0; i < m; i++){
+      for(int j = 0; j < n; j++){	
+	matrix_out[i][j] += matrix1[i][z] * matrix2[z][j];
+      }
+    }
+  }
 }
 
 
 void matmult_knm(int m,int n,int k,double** matrix1,double** matrix2,double** matrix_out){
-	
-	for(int z = 0; z < k; z++){
-		for(int j = 0; j < n; j++){
-			for(int i = 0; i < m; i++){
 
-				matrix_out[i][j] = matrix_out[i][j] + matrix1[i][z] * matrix2[z][j];
-
-			}
-		}
-	}
+  for(int i = 0; i < m; i++){
+    for(int j = 0; j < n; j++){
+      matrix_out[i][j] = 0;
+    }
+  }
+  for(int z = 0; z < k; z++){
+    for(int j = 0; j < n; j++){
+      for(int i = 0; i < m; i++){
+	matrix_out[i][j] += matrix1[i][z] * matrix2[z][j];
+      }
+    }
+  }
 }
 
 
 void matmult_mkn(int m,int n,int k,double** matrix1,double** matrix2,double** matrix_out){
-	
-	for(int i = 0; i < m; i++){
-		for(int z = 0; z < k; z++){
-			for(int j = 0; j < n; j++){
 
-				matrix_out[i][j] = matrix_out[i][j] + matrix1[i][z] * matrix2[z][j];
-
-			}
-		}
-	}
+  for(int i = 0; i < m; i++){
+    for(int j = 0; j < n; j++){
+      matrix_out[i][j] = 0;
+    }
+  }	
+  for(int i = 0; i < m; i++){
+    for(int z = 0; z < k; z++){
+      for(int j = 0; j < n; j++){
+	matrix_out[i][j] +=  matrix1[i][z] * matrix2[z][j];
+      }
+    }
+  }
 }
 
 
 void matmult_nkm(int m,int n,int k,double** matrix1,double** matrix2,double** matrix_out){
+
+  for(int i = 0; i < m; i++){
+    for(int j = 0; j < n; j++){
+      matrix_out[i][j] = 0;
+    }
+  }	
 	
-	for(int j = 0; j < n; j++){
-		for(int z = 0; z < k; z++){
-			for(int i = 0; i < m; i++){
-
-				matrix_out[i][j] = matrix_out[i][j] + matrix1[i][z] * matrix2[z][j];
-
-			}
-		}
-	}
+  for(int j = 0; j < n; j++){
+    for(int z = 0; z < k; z++){
+      for(int i = 0; i < m; i++){	
+	matrix_out[i][j] = matrix_out[i][j] + matrix1[i][z] * matrix2[z][j];
+      }
+    }
+  }
 }
-
-
 
 void matmult_blk(int m, int n, int k, double** matrix1, double** matrix2, double** matrix_out, int step){
 
-	int i_start;
-	int j_start;
-	int z_start;
+  int i_start;
+  int j_start;
+  int z_start;
 	
-	i_start = step * (m / step);
-	j_start = step * (n / step);
-	z_start = step * (k / step);
+  i_start = step * (m / step);
+  j_start = step * (n / step);
+  z_start = step * (k / step);
 
 
-	for(int i_step = 0; i_step < m; i_step += step){
-		
-		for(int j_step = 0; j_step < n; j_step += step){
+  for(int i = 0; i < m; i++){
+    for(int j = 0; j < n; j++){
+      matrix_out[i][j] = 0;
+    }
+  }
+  for(int i_step = 0; i_step < m; i_step += step){
+    
+    for(int j_step = 0; j_step < n; j_step += step){
 			
-			for(int z_step = 0; z_step < k; z_step += step){
-			
-				for(int i = i_step; i < min(i_step + step, m); i++){
-					
-					for(int j = j_step; j < min(j_step + step, n); j++){
+      for(int z_step = 0; z_step < k; z_step += step){
+	
+	for(int i = i_step; i < min(i_step + step, m); i++){
+	  
+	  for(int j = j_step; j < min(j_step + step, n); j++){
 						
-						for(int z = z_step; z < min(z_step + step, k); z++){
+	    for(int z = z_step; z < min(z_step + step, k); z++){
 
-							printf("%d %d %d\n",i,j,z);
-							matrix_out[i][j] = matrix_out[i][j] + matrix1[i][z] * matrix2[z][j];
-
-
-
-						}
-					}
-				}
-			}
-		}
+	      printf("%d %d %d\n",i,j,z);
+	      matrix_out[i][j] = matrix_out[i][j] + matrix1[i][z] * matrix2[z][j];
+	      
+	      
+	      
+	    }
+	  }
 	}
+      }
+    }
+  }
 }
-
-
-
-
